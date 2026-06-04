ad87f0038e1879a33080bea173a981f5429576be
Sameer Saharan <114926999+saharansameer@users.noreply.github.com>
2026-06-04 08:52:58 +0530

    chore: use dyamic year range in footer

--- src/components/Common/CopyrightText.tsx
+++ src/components/Common/CopyrightText.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import { getCurrentFullYear } from "@/lib/utils";
 
 export function CopyrightText() {
   return (
     <div className="text-xs text-muted-foreground font-instrument">
-      <p>&copy; 2025 GhostPOV. All Rights Reserved.</p>
+      <p>&copy; 2025-{getCurrentFullYear()} GhostPOV. All rights reserved.</p>
       <p>
         built by{" "}
         <Link href={"https://sameersaharan.com"} target="_blank" className="p-link">
